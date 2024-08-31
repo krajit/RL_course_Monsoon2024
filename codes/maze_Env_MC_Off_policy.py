af6c41af45cdfdc09704bcce0c1a908097f652db
Sneh Lata <ajitkumar22@gmail.com>
2024-08-31 14:22:18 +0530

    class demo code added

---
+++ codes/maze_Env_MC_Off_policy.py
@@ -0,0 +1,234 @@
+from typing import Tuple, Dict, Iterable, Optional
+
+import numpy as np
+import gym
+from gym import spaces
+import pygame
+from pygame import gfxdraw
+
+
+class Maze(gym.Env):
+
+    def __init__(self, exploring_starts: bool = False,
+                 shaped_rewards: bool = False, size: int = 5) -> None:
+        super().__init__()
+        self.exploring_starts = exploring_starts
+        self.shaped_rewards = shaped_rewards
+        self.state = (size - 1, size - 1)
+        self.goal = (size - 1, size - 1)
+        self.maze = self._create_maze(size=size)
+        self.distances = self._compute_distances(self.goal, self.maze)
+        self.action_space = spaces.Discrete(n=4)
+        self.action_space.action_meanings = {0: 'UP', 1: 'RIGHT', 2: 'DOWN', 3: "LEFT"}
+        self.observation_space = spaces.MultiDiscrete([size, size])
+
+        self.screen = None
+
+    def step(self, action: int) -> Tuple[Tuple[int, int], float, bool, Dict]:
+        reward = self.compute_reward(self.state, action)
+        self.state = self._get_next_state(self.state, action)
+        done = self.state == self.goal
+        info = {}
+        return self.state, reward, done, info
+
+    def reset(self) -> Tuple[int, int]:
+        if self.exploring_starts:
+            while self.state == self.goal:
+                self.state = tuple(self.observation_space.sample())
+        else:
+            self.state = (0, 0)
+        return self.state
+
+    def render(self, mode: str = 'human') -> Optional[np.ndarray]:
+        assert mode in ['human', 'rgb_array']
+
+        screen_size = 600
+        scale = screen_size / 5
+
+        if self.screen is None:
+            pygame.init()  # Initialize Pygame
+            self.screen = pygame.display.set_mode((screen_size, screen_size))
+            pygame.display.set_caption('Maze Environment')
+
+        surf = pygame.Surface((screen_size, screen_size))
+        surf.fill((22, 36, 71))
+
+        for row in range(5):
+            for col in range(5):
+                state = (row, col)
+                for next_state in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                    if next_state not in self.maze[state]:
+                        row_diff, col_diff = np.subtract(next_state, state)
+                        left = (col + (col_diff > 0)) * scale - 2 * (col_diff != 0)
+                        right = ((col + 1) - (col_diff < 0)) * scale + 2 * (col_diff != 0)
+                        top = (5 - (row + (row_diff > 0))) * scale - 2 * (row_diff != 0)
+                        bottom = (5 - ((row + 1) - (row_diff < 0))) * scale + 2 * (row_diff != 0)
+
+                        gfxdraw.filled_polygon(surf, [(left, bottom), (left, top), (right, top), (right, bottom)], (255, 255, 255))
+
+        # Add the geometry of the goal square to the viewer.
+        left, right, top, bottom = scale * 4 + 10, scale * 5 - 10, scale - 10, 10
+        gfxdraw.filled_polygon(surf, [(left, bottom), (left, top), (right, top), (right, bottom)], (40, 199, 172))
+
+        # Add the geometry of the agent to the viewer.
+        agent_row = int(screen_size - scale * (self.state[0] + .5))
+        agent_col = int(scale * (self.state[1] + .5))
+        gfxdraw.filled_circle(surf, agent_col, agent_row, int(scale * .6 / 2), (228, 63, 90))
+
+        surf = pygame.transform.flip(surf, False, True)
+        self.screen.blit(surf, (0, 0))
+
+        pygame.display.flip()
+
+        if mode == 'rgb_array':
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
+            )
+
+    def close(self) -> None:
+        if self.screen is not None:
+            pygame.display.quit()
+            pygame.quit()
+            self.screen = None
+
+    def compute_reward(self, state: Tuple[int, int], action: int) -> float:
+        next_state = self._get_next_state(state, action)
+        if self.shaped_rewards:
+            return - (self.distances[next_state] / self.distances.max())
+        return - float(state != self.goal)
+
+    def simulate_step(self, state: Tuple[int, int], action: int):
+        reward = self.compute_reward(state, action)
+        next_state = self._get_next_state(state, action)
+        done = next_state == self.goal
+        info = {}
+        return next_state, reward, done, info
+
+    def _get_next_state(self, state: Tuple[int, int], action: int) -> Tuple[int, int]:
+        if action == 0:
+            next_state = (state[0] - 1, state[1])
+        elif action == 1:
+            next_state = (state[0], state[1] + 1)
+        elif action == 2:
+            next_state = (state[0] + 1, state[1])
+        elif action == 3:
+            next_state = (state[0], state[1] - 1)
+        else:
+            raise ValueError("Action value not supported:", action)
+        if next_state in self.maze[state]:
+            return next_state
+        return state
+
+    @staticmethod
+    def _create_maze(size: int) -> Dict[Tuple[int, int], Iterable[Tuple[int, int]]]:
+        maze = {(row, col): [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+                for row in range(size) for col in range(size)}
+
+        left_edges = [[(row, 0), (row, -1)] for row in range(size)]
+        right_edges = [[(row, size - 1), (row, size)] for row in range(size)]
+        upper_edges = [[(0, col), (-1, col)] for col in range(size)]
+        lower_edges = [[(size - 1, col), (size, col)] for col in range(size)]
+        walls = [
+            [(1, 0), (1, 1)], [(2, 0), (2, 1)], [(3, 0), (3, 1)],
+            [(1, 1), (1, 2)], [(2, 1), (2, 2)], [(3, 1), (3, 2)],
+            [(3, 1), (4, 1)], [(0, 2), (1, 2)], [(1, 2), (1, 3)],
+            [(2, 2), (3, 2)], [(2, 3), (3, 3)], [(2, 4), (3, 4)],
+            [(4, 2), (4, 3)], [(1, 3), (1, 4)], [(2, 3), (2, 4)],
+        ]
+
+        obstacles = upper_edges + lower_edges + left_edges + right_edges + walls
+
+        for src, dst in obstacles:
+            maze[src].remove(dst)
+
+            if dst in maze:
+                maze[dst].remove(src)
+
+        return maze
+
+    @staticmethod
+    def _compute_distances(goal: Tuple[int, int],
+                           maze: Dict[Tuple[int, int], Iterable[Tuple[int, int]]]) -> np.ndarray:
+        distances = np.full((5, 5), np.inf)
+        visited = set()
+        distances[goal] = 0.
+
+        while visited != set(maze):
+            sorted_dst = [(v // 5, v % 5) for v in distances.argsort(axis=None)]
+            closest = next(x for x in sorted_dst if x not in visited)
+            visited.add(closest)
+
+            for neighbour in maze[closest]:
+                distances[neighbour] = min(distances[neighbour], distances[closest] + 1)
+        return distances
+
+import time
+# Running the environment and displaying the Pygame window
+
+# CODE STARTS RUNNING FROM HERE
+if __name__ == "__main__":
+    env = Maze()
+    state = env.reset()
+    env.render()
+   
+        
+    # placeholder for saving q(s,a)s
+    q = {} 
+
+    # policy function
+    def pi(state, epsilon = 0.1):
+        # is state note visited before return a random action
+        if state not in q:
+            q[state] = {}                      # add state in q
+            action =  env.action_space.sample()   # return a random action
+         
+        elif np.random.rand() < epsilon:            # with epsilon probability return random action
+            action = env.action_space.sample()
+        else:
+            action = max(q[state], key = q[state].get)  # return action with highest value
+        
+        q[state][action] = 0.0
+        return action 
+
+    
+    def generateOneEpisode(wantToView = False):
+        state = env.reset()
+        episode = [(state)]
+        if wantToView:
+            env.render()
+
+    
+        done = False
+        while not done:
+            if wantToView:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+
+            action = pi(state)
+            actionMeaning = env.action_space.action_meanings[action]
+            state, reward, done, _ = env.step(action)
+            episode.append((action,reward,state))
+
+            if wantToView:
+                env.render()  
+                time.sleep(0.01)
+        return episode
+
+    # def generateEpisode():
+    #     state = env.reset()
+    #     episode = [(state)]
+
+    gamma = 0.9
+    alpha = 0.01
+
+    for i in range(20): # generate 20 episodes
+        episode = generateOneEpisode(wantToView=False)
+        C = 0
+        for (action, reward,state) in reversed(episode[1:]):
+            C = reward + gamma*C
+            q[state][action] +=alpha*(C -q[state][action] ) 
+
+
+    generateOneEpisode(wantToView=True)
+    print("done")
