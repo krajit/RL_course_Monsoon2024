92a345c1f987f109ef31782c3317de57776a0b72
Sneh Lata <ajitkumar22@gmail.com>
2024-09-07 13:23:57 +0530

    Update basicPythonTraining.py

--- codes/basicPythonTraining.py
+++ codes/basicPythonTraining.py
@@ -4,13 +4,26 @@ theta = 1e-5
 gamma = 0.9
 
 import numpy as np
-
+import maze
 
 # V will be save in a dictionary
 V = {}
 for i in range(5):
     for j in range(5):
         V[(i,j)] = -10*np.random.rand()
+V[(4,4)] = 0
+
+##  HW
+# Implemment line 3 to 11
+
+
+env = maze.Maze()
+env.reset()
+env.render()
+
+#  helpful function
+#env.simulate_step()
 
 
 print("done")
+
