ea9a15bb8d50f79cf715bd414d1bfe464d782959
Ubuntu <ubuntu@ip-172-31-5-22.ap-south-1.compute.internal>
2024-11-14 02:30:10 +0000

    ray logs being saved in local dir

--- deeprl_rllib_examples/cartpole/step_1_training_tune_api.py
+++ deeprl_rllib_examples/cartpole/step_1_training_tune_api.py
@@ -2,6 +2,7 @@
 from ray.rllib.algorithms.ppo import PPOConfig
 import numpy as np
 from ray import tune, air
+import os
 
 # Configure PPO with a learning rate grid search
 config = (
@@ -9,6 +10,11 @@ config = (
     .environment(env="CartPole-v1")
     .training(lr=tune.grid_search([0.01, 0.001, 0.0001]))
 )
+
+log_dir = os.path.join(os.getcwd(), "deeprl_rllib_examples/cartpole/ray_logs")
+
+# Create the directory if it doesn't exist
+os.makedirs(log_dir, exist_ok=True)
 
 # Set up the tuner with a stop criterion and checkpoint configuration
 tuner = tune.Tuner(
@@ -18,7 +24,8 @@ tuner = tune.Tuner(
         stop={"env_runners/episode_reward_mean": 300.0},
         checkpoint_config=air.CheckpointConfig(
             checkpoint_at_end=True  # Saves a checkpoint at the end of training
-        )
+        ),
+        storage_path=log_dir
     ),
 )
 
--- deeprl_rllib_examples/cartpole/step_2_running_trained_model.py
+++ deeprl_rllib_examples/cartpole/step_2_running_trained_model.py
@@ -1,7 +1,9 @@
 #path = "/home/ubuntu/ray_results/PPO_2024-11-11_08-40-15/PPO_CartPole-v1_93a16_00000_0_lr=0.0100_2024-11-11_08-40-16/checkpoint_000000"
-path = "/home/ubuntu/ray_results/PPO_2024-11-12_09-22-42/PPO_CartPole-v1_ac66e_00002_2_lr=0.0001_2024-11-12_09-22-44/checkpoint_000000"
+#path = "/home/ubuntu/ray_results/PPO_2024-11-12_09-22-42/PPO_CartPole-v1_ac66e_00002_2_lr=0.0001_2024-11-12_09-22-44/checkpoint_000000"
 #import gym
+import os
 
+path = os.path.join(os.getcwd(),"deeprl_rllib_examples/cartpole/ray_logs/PPO_2024-11-14_02-03-47/PPO_CartPole-v1_b048f_00002_2_lr=0.0001_2024-11-14_02-03-49/checkpoint_000000")
 
 
 from ray.rllib.algorithms.algorithm import Algorithm
