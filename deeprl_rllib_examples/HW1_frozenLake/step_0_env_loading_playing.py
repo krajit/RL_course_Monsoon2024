933db7d7529b7963061cf90fc0fca856db85b7fc
Ubuntu <ubuntu@ip-172-31-12-117.ap-south-1.compute.internal>
2024-11-17 06:48:33 +0000

    minor changes

--- deeprl_rllib_examples/HW1_frozenLake/step_0_env_loading_playing.py
+++ deeprl_rllib_examples/HW1_frozenLake/step_0_env_loading_playing.py
@@ -12,14 +12,17 @@ def create_env(config):
     env = gym.make( 'FrozenLake-v1', render_mode="human",desc= desc, map_name=desc, is_slippery=False)
     return env 
 
-#env = create_env(config=config)
-env = gym.make( 'FrozenLake-v1', render_mode="human")
 
-state = env.reset()
-done = False
-while not done:
-    action = env.action_space.sample()
-    next_state, reward, done,_, extra_info = env.step(action)
-    env.render()
+
+for i in range(10):
+    env = create_env(config=config)
+    #env = gym.make( 'FrozenLake-v1', render_mode="human")
+
+    state = env.reset()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        next_state, reward, done,_, extra_info = env.step(action)
+        env.render()
     
 print("done")
